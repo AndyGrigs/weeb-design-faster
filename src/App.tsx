b4d0e2a1fdc8ed609e5e6a7322ca88a932ae4706
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-03-02 15:56:15 +0100

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import CarList from './components/CarList';
 
 function App() {
   return (
-    
-    
     <Router>
-      <CarList/>
-      <div>
-      {/* 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav> */}
-
-        {/* <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
-        </Routes> */}
-      </div>
     </Router>
   );
 }
 
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
 
 export default App;
