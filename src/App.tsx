fd1fe57a30ffea919b078c0e2ee4458b22b556b3
AndyGrigs <andygrigs88@gmail.com>
2025-03-03 13:21:03 +0100

    wrapper styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function App() {
   return (
-   <section className="page">
-      <div className="page__container">
-      Am Freitag, dem 08.11.2024, rangelte Julian mit drei anderen Klassenkameraden in der Pause. Auslöser war das Wegnehmen der Schildmütze eines Mitschülers. Es stellte sich schnell ein ungünstiges Machtgefälle heraus: Drei Jungen gegen einen (Mützenbesitzer). Diesem Jungen wurde immer wieder seine Mütze genommen, bis sich die vier Jungen am Ende balgten. 
-    Julian hielt dabei seinen Arm ungünstig am Hals des Geschädigten, sodass dieser aktiv Luftnot bekam. Der Geschädigte seinerseits verdrehte Julian das Knie. Beide Jungen entschuldigten sich gegenseitig und zeigten Reue. In einer ähnlichen Situation wollen sie zukünftig angemessener reagieren (sich von Gruppe entfernen, zur Deeskalation beitragen, Aufsicht zur Hilfe holen) und die Hausordnung einhalten. 
-
+  
+      <div className="wrapper">
+        <header className="header">Header</header>
+        <main className="main">Main</main>
+        <footer className="footer">Footer</footer>
       </div>
-   </section>
+ 
   );
 }
 
